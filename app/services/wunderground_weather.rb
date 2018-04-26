ab30a37bfede64fdb295002fe8e6ed299d967c00
Erik the Viking <erik@hhd.com.au>
2018-04-26 15:16:26 +1000

    add some strucutre for wunderground service

--- app/services/wunderground_weather.rb
+++ app/services/wunderground_weather.rb
@@ -1,4 +1,16 @@
-module WundergroundWeather
-  def self.call
+class WundergroundWeather
+
+  def initialize(serializer)
+    @serializer = serializer
   end
+
+  def call(country, city)
+  end
+
+  private
+  def endpoint
+    secrets = Rails.application.secrets
+    "http://api.wunderground.com/api/f1860d746ba9953d/geolookup/conditions/forecast/q/#{country}/#{city}.json"
+  end
+
 end
---
+++ todo.md
@@ -0,0 +1,3 @@
+- finish writing wunderground_weather service
+- write serializer to handle response form wunderground enpoint
+
